c8469c5517c81bced69b59276c206d52d464e743
AhmetZeer <ahmed.zeer@std.yildiz.edu.tr>
2026-03-14 15:07:16 +0300

    Initial commit

---
+++ source/uav_lab_1/uav_lab_1/tasks/direct/uav_lab_1_marl/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Uav-Lab-1-Marl-Direct-v0",
+    entry_point=f"{__name__}.uav_lab_1_marl_env:UavLab1MarlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.uav_lab_1_marl_env_cfg:UavLab1MarlEnvCfg",
+        "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_cfg.yaml",
+        "skrl_mappo_cfg_entry_point": f"{agents.__name__}:skrl_mappo_cfg.yaml",
+    },
+)
